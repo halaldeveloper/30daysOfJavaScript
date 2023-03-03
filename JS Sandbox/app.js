d6d6e378e34a1ae64538e84e68db2d879852d3b7
halaldeveloper <issahpeacee@gmail.com>
2023-03-03 09:01:20 +0100

    halalDevelopers Tech Journey - 30DAYS OF JAVASCRIPT IN MARCH

---
+++ JS Sandbox/app.js
@@ -0,0 +1,12 @@
+const techJourney = 'JavaScriptIn30days';
+const halalDeveloper = 'halalDeveloper';
+
+let val;
+
+val = halalDeveloper.concat(' learnt ', techJourney)
+
+if(techJourney === 'JavaScriptIn30days') {
+    console.log(`${val}`);
+} else {
+    console.log('BE CONSISTENT!');
+};
