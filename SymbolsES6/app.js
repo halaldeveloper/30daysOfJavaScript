dca2cc5fa15ff6971de26699f682934a4ae81246
halaldeveloper <issahpeacee@gmail.com>
2023-04-05 10:45:09 +0100

    Symbols in JavaScript lesson

---
+++ SymbolsES6/app.js
@@ -0,0 +1,26 @@
+// Every symbol is completely unique, and makes them very valuable in terms of object property identifiers
+
+// Create a symbol
+// const sym1 = Symbol();
+
+// Unique Object keys
+const KEY1 = Symbol();
+const KEY2 = Symbol('sym2');
+
+const myObj = {};
+
+myObj[KEY1] = 'Prop1';
+myObj[KEY2] = 'Prop2';
+myObj.key3 = 'Prop3';
+myObj.key4 = 'Prop4';
+
+// console.log(myObj[KEY1]);
+
+// Symbols are not enumerable in for...in
+for(let i in myObj) {
+    // console.log(`${i}: ${myObj[i]}`);
+}
+
+// Symbols are ignored by JSON.stringify
+console.log(JSON.stringify({key: 'prop'}));
+console.log(JSON.stringify({[Symbol('sym1')]: 'prop'}));
